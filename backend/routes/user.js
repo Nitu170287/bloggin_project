6b09c76bfc8666a31aedecaac170ea425b8ed1bc
Nitu Dubey <nitutiwari01@gmail.com>
2023-10-26 21:40:50 -0400

    removed frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,6 @@
+const express = require("express");
+const app = express();
 
-const express = require('express')
-const app = express()
+app.post("/register", registerUser);
 
-app.post("/register", registerUser)
-
-module.exports = app
+module.exports = app;
